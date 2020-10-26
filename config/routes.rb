339022b83a6becac89895fc90638c4c6a67cfefa
hamaa-affix <hamaagive1108@gmail.com>
2020-10-26 11:04:03 +0900

    tutorial section5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
   root 'static_pages#home'
+  #get 'controller#methods'
+  get 'static_pages/home'
+  get '/help_path', to: 'static_pages#help', as:'helf'
+  get '/about_path', to: 'static_pages#about', as:'about'
+  get '/contact_path', to: 'static_pages#contact', as:'contact'
+  #anthor demo
+  get '/boards', to:'boards#index', as:'boards'
   # Roure::get('usl', 'controller@method_name');laravel
   # rootとすることでdefaultrootingの設定ができる。/ が来たらusersのindexControllerが呼び出されviewへ投げられる
   #root 'users#index'
